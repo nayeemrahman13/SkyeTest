4c67879dc0582130439e1805cb00e52b30501d38
nayeemrahman13 <mmr548@nyu.edu>
2023-06-04 01:48:35 +0000

    Added trip detail function and trip detail html page

--- main.py
+++ main.py
@@ -9,5 +9,13 @@ def index():
   username = request.headers['X-Replit-User-Name']
   return render_template('index.html', username = username)
 
+@app.route('/trip/<trip_id>')
+def trip_details(trip_id):
+  # Fetch the trip information from the database based on the trip ID
+  trip_data = db[trip_id]
+
+  # Render the trip details template with the trip information
+  return render_template('trip_details.html', trip_data=trip_data)
+
 app.run(host='0.0.0.0', port=81)
 
